04a6d54dd53efd2941d9f4749c019299d1d68fc0
TallonsCZ <tallonscze@gmail.com>
2023-04-19 20:26:15 +0200

    Add not working new Item

---
+++ src/main/java/me/tallonscz/mostutilities/item/ModItems.java
@@ -0,0 +1,24 @@
+package me.tallonscz.mostutilities.item;
+
+import me.tallonscz.mostutilities.MostUtilities;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MostUtilities.MODID);
+
+    public static final RegistryObject<Item> MOD_STICK = ITEMS.register("mod_stick",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
